b10d1506058847f33b0bb6790f794feb556d1f9a
Mou Chen <chenmou929@gmail.com>
2019-07-19 05:43:29 -0400

    Require authentication

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// const mongoose = require('mongoose');
+// mongoose.connect(
+//   ``,
+//   { useNewUrlParser: true }
+// );
+
+// var db = mongoose.connection;
+// db.on('error', err => console.error(err));
+// db.once('open', () => console.log('Connected to Mongodb'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
